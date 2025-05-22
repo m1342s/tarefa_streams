0ce1685a33c74a5e3d072ccbcdc7a17ce9851869
Matheus Viersa <matheusviersa995@hotmail.com>
2025-05-22 11:03:31 -0300

    Tarefa a ser verificada

--- main/java/streams/ClassificaPessoas.java
+++ main/java/streams/ClassificaPessoas.java
@@ -1,9 +1,7 @@
 package streams;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ClassificaPessoas {
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module tarefa_streams1 {
+	requires junit;
 }
